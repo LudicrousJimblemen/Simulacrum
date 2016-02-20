0c5911d910adc0d7b5a2da6e780c12bdccbb6bc0
Poyo <poyoarya@gmail.com>
2016-02-19 21:55:21 -0800

    Person 1.0

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person : MonoBehaviour {
+
+    public Animator personAnimator;
+
+    // Use this for initialization
+    void Start() {
+        personAnimator.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update() {
+        personAnimator.SetBool("spaceKeyDown", Input.GetKeyDown(KeyCode.Space));
+        personAnimator.SetBool("wKeyHeld", Input.GetKey(KeyCode.W));
+    }
+}
