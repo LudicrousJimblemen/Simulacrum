816d2a1b3cca09d734dd25abcd76bde74d00845b
Avekeez <s-ssoetomo@lwsd.org>
2016-02-26 14:31:31 -0800

    added funny walking

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -1,18 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Person : MonoBehaviour {
 
     public Animator personAnimator;
+    public GameObject Selector;
+    bool inWalk = false;
+    Vector3 walkStart;
+    Vector3 walkEnd;
+    float walkStartTime;
 
     // Use this for initialization
-    void Start() {
+    void Awake() {
         personAnimator.enabled = true;
     }
 	
 	// Update is called once per frame
 	void Update() {
         personAnimator.SetBool("spaceKeyDown", Input.GetKeyDown(KeyCode.Space));
-        personAnimator.SetBool("wKeyHeld", Input.GetKey(KeyCode.W));
+        personAnimator.SetBool("wKeyHeld", inWalk);
+        if (inWalk) {
+        	float distance = Vector3.Distance (walkStart, walkEnd);
+        	transform.position = Vector3.Lerp (walkStart, walkEnd, (Time.time-walkStartTime)*5/distance);
+        	if (transform.position == walkEnd) inWalk = false;
+        	transform.LookAt (walkEnd);
+        }
     }
+	
+	public void startWalk (Vector3 goal) {
+		print (goal);
+		walkStart = transform.position;
+		walkEnd = goal;
+		walkStartTime = Time.time;
+		inWalk = true;
+	}
 }
---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Select : MonoBehaviour {
+	public Vector3 position;
+	public Vector3 selection;
+	GameObject persons;
+	void Awake () {
+		persons = GameObject.Find ("Persons");
+	}
+	
+	void Update () {
+		Ray ray = new Ray (Camera.main.ScreenToWorldPoint (Input.mousePosition), transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit) ){
+			position = hit.point;
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			selection = position;
+			movePersons ();
+		}
+	}
+	void OnDrawGizmosSelected () {
+		Gizmos.DrawSphere (selection, 1);
+	}
+	void movePersons () {
+		for  (int i = 0; i < persons.transform.childCount; i++) {
+			GameObject obj = persons.transform.GetChild (i).gameObject;
+			obj.GetComponent<Person> ().startWalk (selection);
+		}
+	}
+}
