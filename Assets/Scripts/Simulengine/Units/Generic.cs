ab1e7518b60edddb2a3171bd03117829823dcd22
Poyo <poyoarya@gmail.com>
2016-03-05 20:54:46 -0800

    Selection changes materials rather than showing sphere

--- Assets/Scripts/Simulengine/Units/Generic.cs
+++ Assets/Scripts/Simulengine/Units/Generic.cs
@@ -2,10 +2,20 @@
 using System.Collections;
 
 public class Generic : MonoBehaviour {
-	public bool Selected = false;
+	public bool Selected;
 	public int[] Resources = {0, 0, 0};
-	
-	void Update () {
-		transform.GetChild (2).gameObject.SetActive (Selected);
+	public Material defaultMaterial;
+	public Material outlineMaterial;
+
+	void Start() {
+		//'Sup?
+    }
+
+	void Update() {
+		if (Selected) {
+			GetComponentInChildren<SkinnedMeshRenderer>().material = outlineMaterial;
+		} else {
+			GetComponentInChildren<SkinnedMeshRenderer>().material = defaultMaterial;
+		}
 	}
 }
