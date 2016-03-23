a6ab0d52edc797fda60c89e47b9135a12c952ab6
Poyo <poyoarya@gmail.com>
2016-03-22 21:18:58 -0700

    maybe network

---
+++ Assets/Scripts/Simulengine/Network/NetworkManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+class NetworkManager : MonoBehaviour {
+	private const string typeName = "JimblemenSimulacrum";
+	private const string gameName = "bumgame";
+
+	private HostData[] hostList;
+
+	private void StartServer() {
+		Network.InitializeServer(2, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+
+	void OnServerInitialized() {
+		Debug.Log("Server Initializied");
+	}
+
+	void OnConnectedToServer() {
+		Debug.Log("Server Connected To");
+	}
+
+	private void RefreshHostList() {
+		MasterServer.RequestHostList(typeName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent) {
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData) {
+		Network.Connect(hostData);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.N) &&
+			!Network.isClient &&
+			!Network.isServer) {
+			StartServer();
+		}
+		if (Input.GetKeyDown(KeyCode.R) &&
+			!Network.isClient &&
+			!Network.isServer) {
+			RefreshHostList();
+
+			if (hostList != null) {
+				string s = " ";
+				for (int i = 0; i < hostList.Length; i++) {
+					s += hostList[i].gameName + " ";
+				}
+				Debug.Log(s);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.J) &&
+			!Network.isClient &&
+			!Network.isServer) {
+			RefreshHostList();
+
+			if (hostList != null) {
+				StartServer();
+			}
+		}
+	}
+}
