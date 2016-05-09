d3cd676b20e79f1057a8a3bcca4938a57ac5b456
Poyo <poyoarya@gmail.com>
2016-05-09 16:06:20 -0700

    Better mesh generation

--- Assets/Scripts/Simulengine/Terrain/MapDisplay.cs
+++ Assets/Scripts/Simulengine/Terrain/MapDisplay.cs
@@ -2,10 +2,20 @@
 using System.Collections;
 
 public class MapDisplay : MonoBehaviour {
+
 	public Renderer textureRender;
+	public MeshFilter meshFilter;
+	public MeshRenderer meshRenderer;
 
 	public void DrawTexture(Texture2D texture) {
 		textureRender.sharedMaterial.mainTexture = texture;
-		textureRender.transform.localScale = new Vector3 (texture.width / 4, 1, texture.height / 4);
+		textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height);
 	}
+
+	public void DrawMesh(MeshData meshData, Texture2D texture) {
+		meshFilter.sharedMesh = meshData.CreateMesh ();
+		meshRenderer.sharedMaterial.mainTexture = texture;
+	}
+
 }
+
